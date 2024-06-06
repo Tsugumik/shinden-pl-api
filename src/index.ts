6085a65da87e0712c8dbb594d4c34797c861ae0a
Błażej Drozd <drozdbazej@gmail.com>
2024-06-06 16:23:23 +0200

    Add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
 export { Anime } from './utils/Anime.js';
+export { AnimeStats } from './utils/AnimeStats.js';
+export { AnimeDetails } from './utils/AnimeDetails.js';
+export { AnimeType } from './utils/AnimeType.js';
+export { AnimeStatus } from './utils/AnimeStatus.js';
+export { Episode } from './utils/Episode.js';
+export { Player } from './utils/Player.js';
 export { SearchAnime } from './SearchAnime.js';
+
+
+// Fetching
+export { Fetcher } from './utils/fetching/Fetcher.js';
+export { PageType } from './utils/fetching/PageType.js';
+export { verifyRequest } from './utils/fetching/verifyRequest.js';
