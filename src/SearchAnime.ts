16d8e49b1b514a3ef8930e009f7e6baecacca875
Błażej Drozd <drozdbazej@gmail.com>
2024-06-07 14:47:40 +0200

    Add function to search anime

--- src/SearchAnime.ts
+++ src/SearchAnime.ts
@@ -1,4 +1,7 @@
-export async function SearchAnime(name: string): Promise<Array<string>> {
-    
-    return;
+import { SearchPage } from "./utils/SearchPage.js";
+import { Fetcher } from "./utils/fetching/Fetcher.js";
+
+export async function searchAnime(name: string): Promise<SearchPage> {
+    const HTML = await Fetcher.fetchSearchPage(name);
+    return new SearchPage(name, null, HTML);
 }
